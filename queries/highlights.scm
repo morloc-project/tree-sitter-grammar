c5bf1f06cd82099611ce461ff44e8dac61ce00d7
Zebulun Arendsee <zbwrnz@gmail.com>
2025-08-02 18:45:09 -0400

    Update highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -11,53 +11,37 @@
 
 (lineComment) @comment
 (blockComment) @comment
-(tag) @comment
+(tag) @keyword.directive
 
-"=" @operator
-"->" @operator
-"=>" @operator
-"::" @operator
-"." @operator
+[
+  "="
+  "->"
+  "=>"
+  "::"
+  "." 
+] @operator
 
-"where" @keyword
-"module" @keyword
-"import" @keyword
-"source" @keyword
-"from" @keyword
-"as" @keyword
-"type" @keyword
+[
+  "module"
+  "import"
+  "source"
+  "from"
+  "as"
+] @keyword.import
+
+[
+  "where"
+  "type"
+] @keyword
+
 
 "," @punctuation.delimiter
-"{" @punctuation.bracket
-"}" @punctuation.bracket
-"[" @punctuation.bracket
-"]" @punctuation.bracket
-"(" @punctuation.bracket
-")" @punctuation.bracket
 
-; Capture Name                 Meaning
-; @keyword                     Language keywords
-; @function                    Function names or declarations
-; @function.builtin            Built-in functions
-; @type                        Types (classes, structs, enums)
-; @type.builtin                Built-in types (int, float, etc.)
-; @variable                    Variable names
-; @variable.builtin            Built-in variables or constants
-; @variable.parameter          Function or method parameters
-; @property                    Object properties
-; @property.builtin            Built-in properties
-; @constant                    Constants
-; @constant.builtin            Built-in constants
-; @string                      String literals
-; @string.special              Special string formats (e.g. regex)
-; @number                      Numeric literals
-; @comment                     Comments
-; @operator                    Operators
-; @punctuation                 Punctuation characters
-; @punctuation.bracket         Brackets (parentheses, braces)
-; @punctuation.delimiter       Delimiters (comma, semicolon)
-; @tag                         Tags (for XML, HTML, etc.)
-; @attribute                   Attributes
-; @parameter                   Function parameters
-; @namespace                   Namespaces
-; @label                       Labels (goto targets)
+[
+  "{"
+  "}"
+  "["
+  "]"
+  "("
+  ")"
+] @punctuation.bracket
